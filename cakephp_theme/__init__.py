dd6c1d47397e99bd3582b4ec2b4200df213a9f38
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2017-01-02 04:41:57 -0700

    feat: add branch to doc context

--- cakephp_theme/__init__.py
+++ cakephp_theme/__init__.py
@@ -11,9 +11,11 @@ def get_html_theme_path():
 
 def update_context(app, pagename, templatename, context, doctree):
     context['cakephp_theme_version'] = __version__
+    context['branch'] = app.config.branch
 
 
 def setup(app):
     app.connect('html-page-context', update_context)
+    app.add_config_value('branch', '', True)
     return {'version': __version__,
             'parallel_read_safe': True}
